a40551d6d76d8917e1a3646711081e00e877150f
mukhtorov <mukhtorov_1994@mail.ru>
2021-12-09 23:57:02 +0500

    birinchi dars

---
+++ review.js
@@ -0,0 +1,11 @@
+// Birinchi dars
+
+// npx create-react-app
+// package.json, src, public
+// single page applicate
+// virtual ReactDOM
+// ReactDOM.render()
+// jsx
+// import
+
+// props, components
