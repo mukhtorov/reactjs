79d2eb03b2de2eb95f8788259e7b3ea708717aef
mukhtorov <mukhtorov_1994@mail.ru>
2021-12-13 00:50:58 +0500

    fuits project

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Student from './Student.jsx';
+import Fruits from './fruits';
 
 const students = [
   { id: 1, name: 'Sardor' },
@@ -17,9 +18,10 @@ const students = [
 
 ReactDOM.render(
   <React.StrictMode>
-    {students.map((value) => {
+    <Fruits />
+    {/* {students.map((value) => {
       return <Student data={value} />;
-    })}
+    })} */}
   </React.StrictMode>,
   document.getElementById('root')
 );
