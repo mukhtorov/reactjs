a630bcdd5017bda7f06fba48dd56bde026bfc11d
mukhtorov <mukhtorov_1994@mail.ru>
2022-02-06 23:47:38 +0500

    lists and keys

--- src/Render.jsx
+++ src/Render.jsx
@@ -1,26 +1,37 @@
 import React, { Component } from 'react';
-import Signup from './Signup';
-import Signin from './Signin';
 
+const list = [
+  { id: 1, title: 'WebBrain Academy' },
+  { id: 2, title: 'Digital One ' },
+  { id: 3, title: 'Top Class Academy' },
+  { id: 4, title: 'WebBrain Academy' },
+  { id: 5, title: 'Share Academy' },
+  { id: 6, title: 'WBA Academy' },
+];
 export default class Render extends Component {
   constructor(props) {
     super(props);
     this.state = {
       active: false,
+      list: list,
     };
   }
   render() {
-    const getRes = (value) => {
-      console.log(value);
-      this.setState({ active: value });
+    const onDelete = (id) => {
+      let res = this.state.list.filter((value) => value.id !== id);
+      this.setState({ list: res });
     };
     return (
-      <div className='container'>
-        {this.state.active ? (
-          <Signin getRes={getRes} />
-        ) : (
-          <Signup getRes={getRes} />
-        )}
+      <div className=''>
+        <h1>List</h1>
+        {this.state.list.map((value) => (
+          <div key={value.id}>
+            <h1>
+              {value.id} {value.title}{' '}
+              <button onClick={() => onDelete(value.id)}>delete</button>
+            </h1>
+          </div>
+        ))}
       </div>
     );
   }
