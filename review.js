b196c9e0b0bbff8f7e528cdf1d28c82ae91ac36d
mukhtorov <mukhtorov_1994@mail.ru>
2022-04-08 23:30:28 +0500

    localStorage

--- review.js
+++ review.js
@@ -113,3 +113,12 @@
 
 // =======================
 // =======================
+// LocalStorage
+// localStorage.setItem(name,value)
+// localStorage.getItem(name)
+// localStorage.removeItem(name)
+// localStorage.clear()
+
+// array, object
+// JSON.stringify(value)
+// JSON.parse(localStorage.getItem('count'))
--- src/components/Hooks.jsx
+++ src/components/Hooks.jsx
@@ -1,28 +1,25 @@
-import React from 'react';
-import { Student } from '../context/Students';
+import React, { useState } from 'react';
 
 const Body = () => {
-  // const [students, setStudents] = useState();
-  const [students, setStudent] = Student();
+  const [count, setCount] = useState(Number(localStorage.getItem('count')));
 
-  const onDelete = (id) => {
-    let res = students.filter((st) => {
-      return st.id !== id;
-    });
-    setStudent(res);
+  const plus = () => {
+    localStorage.setItem('count', JSON.stringify({ data: count + 1 }));
+    setCount(count + 1);
   };
-
+  const minus = () => {
+    setCount(count - 1);
+  };
+  console.log(JSON.parse(localStorage.getItem('count')), 'count');
   return (
-    <div style={{ background: 'coral' }}>
-      <h1>Students List {students.length}</h1>
-      {students.map((student) => {
-        return (
-          <h1 key={student.id}>
-            Name: {student.name}{' '}
-            <button onClick={() => onDelete(student.id)}>delete</button>{' '}
-          </h1>
-        );
-      })}
+    <div>
+      <h1>Counter {count}</h1>
+      <button onClick={plus}>+</button>
+      <button onClick={minus}>-</button>
+      <button onClick={() => localStorage.removeItem('count')}>
+        delete count
+      </button>
+      <button onClick={() => localStorage.clear()}>clear</button>
     </div>
   );
 };
